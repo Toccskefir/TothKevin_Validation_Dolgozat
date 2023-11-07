00fd1ffa5b167edb6a47c89ed264ee6a6dbe846d
Tóth Kevin <tothkevin0716@gmail.com>
2023-11-07 13:28:39 +0100

    first commit

---
+++ src/main.ts
@@ -0,0 +1,39 @@
+import { Artwork } from './Artwork';
+import { Statue } from './Statue';
+
+export const list: Artwork[] = [];
+
+document.addEventListener("DOMContentLoaded", () => {
+  const formStatue = document.getElementById("formStatue") as HTMLFormElement;
+  const errorMessage = document.getElementById("errorMessage");
+  let sumArtwork: number = 0;
+  let sumPrice: number = 0;
+
+  formStatue!.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const title: string = (document.getElementById("inputTitle") as HTMLInputElement).value;
+    const year: number = parseInt((document.getElementById("inputYear") as HTMLInputElement).value);
+    const price: number = parseInt((document.getElementById("inputPrice") as HTMLInputElement).value);
+    const height: number = parseInt((document.getElementById("inputHeight") as HTMLInputElement).value);
+
+    try {
+
+      const newStatue: Statue = new Statue(title, year, price, height);
+      list.push(newStatue);
+
+      formStatue.reset();
+      errorMessage!.innerHTML = "";
+
+      sumArtwork++;
+      sumPrice += price;
+      document.getElementById("amountOfArtwork")!.textContent = "Művek darabszáma: " + sumArtwork;
+      document.getElementById("priceOfArtwork")!.textContent = `Művek összesített ára: ${sumPrice}Ft`;
+
+    } catch(e) {
+      if (e instanceof Error) {
+        errorMessage!.textContent = e.message;
+      }
+    }
+  });
+});
---
+++ src/statue.ts
@@ -0,0 +1,28 @@
+import { Artwork } from './Artwork';
+
+export class Statue implements Artwork {
+    title: string;
+    year: number;
+    price: number;
+    height: number;
+
+    private regexTitle = /^[a-z ,]+$/i;
+    private currentYear = new Date().getFullYear();
+
+    constructor(title: string, year: number, price: number, height: number) {
+        if (!this.regexTitle.test(title)) {
+            throw Error("Hibás szobor cím!");
+        } else if (year > this.currentYear || isNaN(year)) {
+            throw Error("Készítésének éve érvénytelen!");
+        } else if (price < 1 || isNaN(price)) {
+            throw Error("A szobor ára minimum 1Ft kell legyen!");
+        } else if (height < 10 || isNaN(height)) {
+            throw Error("A szobor magassága minimum 10cm kell legyen!");
+        } else {
+        this.title = title;
+        this.year = year;
+        this.price = price;
+        this.height = height;
+        }
+    }
+}
